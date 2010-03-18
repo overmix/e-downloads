c5fe911db4fe074ee4ee84dac33128f520068526
ldmotta <ldmotta@visie.com.br>
2010-03-18 11:38:21 -0300

    Adicionando o config.sample

---
+++ code/app/config.sample.php
@@ -0,0 +1,10 @@
+<?php
+define ('DS', DIRECTORY_SEPARATOR);
+
+// definições de url
+define ('BASE_URL', 'http://localhost/e-downloads');
+
+// definições de path
+define ('BASE_PATH', dirname(dirname(__FILE__)));
+define ('TEMPLATE_PATH', BASE_PATH . DS . 'template' . DS);
+
