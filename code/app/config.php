58e901d8cd496c3f932d86fd3275e9b98370a3e1
ldmotta <ldmotta@visie.com.br>
2010-03-18 15:21:54 -0300

    Criando a model e o config

--- code/app/config.php
+++ code/app/config.php
@@ -2,9 +2,12 @@
 define ('DS', DIRECTORY_SEPARATOR);
 
 // definições de url
-define ('BASE_URL', 'http://localhost/e-downloads');
+define ('BASE_URL', 'http://localhost/e-downloads/code/');
 
 // definições de path
 define ('BASE_PATH', dirname(dirname(__FILE__)));
 define ('TEMPLATE_PATH', BASE_PATH . DS . 'template' . DS);
 
+define('DB_DNS', 'mysql:host=localhost;dbname=edownload');
+define('DB_USERNAME', 'edownload');
+define('DB_PASSWORD', 'edownload');
--- code/home.php
+++ code/home.php
@@ -1,8 +1,15 @@
 <?php
 include 'ice/app.php';
+include 'ice/model.php';
 include 'app/config.php';
+include 'app/model.php';
 include 'app/controllers.php';
 
 app (array(
-    '^/?$' => 'Home',
+    // Cadastro do usuario
+    '^/cadastro/?$'     => 'Cadastro',
+    '^/downloads?/?$'   => 'Download',
+
+    // Deve estar sempre por último
+    '^/?$'              => 'Home',
 ));
