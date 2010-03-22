5ec12ce0a3e27438af74750f73f5cfa49f58fbbd
ldmotta <ldmotta@visie.com.br>
2010-03-22 11:25:15 -0300

    Removendo tudo pra refazer com codeigniter

--- code/app/model.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Usuario extends Auth
-{}
--- code/template/home.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php';?>
-
-<h1><?php echo $titulo;?></h1>
-
-<?php the_msg($msg = null);?>
-
-<?php include 'footer.php';?>
